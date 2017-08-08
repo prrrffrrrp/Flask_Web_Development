dc834e7ebc4732eac68e380aa2c085fc78cc4442
Prrrf <prrrffrrrp@gmail.com>
2017-08-08 09:34:15 +0200

    Using session, redirect and url_for to reload the page without odd message

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, url_for
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -19,12 +19,11 @@ class NameForm(FlaskForm):
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route("/info")
