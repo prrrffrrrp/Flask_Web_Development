9db6f83a96580899cfb2619b2e9a5463aed24a59
Prrrf <prrrffrrrp@gmail.com>
2017-08-08 06:18:22 +0200

    Made templates with flask_bootstrap base and added error handler pages

--- hello.py
+++ hello.py
@@ -1,17 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from datetime import datetime
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route("/")
 def index():
+    return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.route("/info")
+def info():
     user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
-#     return '<h1>Hello World!</h1>'
+    return render_template('info.html', user_agent=user_agent)
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
