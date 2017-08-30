7b545585d390549a7ca5c763802416ab5ef5cafb
prrrf <prrrffrrrp@gmail.com>
2017-08-30 07:42:05 +0200

    Users can change email address

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -35,9 +35,39 @@ class RegistrationForm(FlaskForm):
 
 
 class ChangePasswordForm(FlaskForm):
-    old_password = PasswordField('Password', validators=[Required()])
+    old_password = PasswordField('Old password', validators=[Required()])
     new_password = PasswordField('New Password', validators=[
         Required(), EqualTo('new_password2', message='Passwords must match.')])
     new_password2 = PasswordField('Confirm New Password',
                                   validators=[Required()])
     submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('New Password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Reset Password')
+
+    def validate_email(self, field):
+        if not User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email not registered.')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[Required(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
