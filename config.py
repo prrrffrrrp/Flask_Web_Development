bad8ab6d02e0a566b4dd92a65c8d84a48315b5a6
Prrrf <prrrffrrrp@gmail.com>
2017-09-21 20:09:58 +0200

    Enlarge testing scope with Flask test client

--- config.py
+++ config.py
@@ -33,6 +33,7 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WTF_CRSF_ENABLED = True
 
 
 class ProductionConfig(Config):
